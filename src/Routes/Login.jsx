022c4f897304952544617b15707011b8dc1ddaa7
Mozarth Spier <mozarthspier@gmail.com>
2022-12-15 21:45:52 -0300

    Remove console.log()

--- src/Routes/Login.jsx
+++ src/Routes/Login.jsx
@@ -3,7 +3,6 @@ import { useDentistInfo } from "./../Hooks/useDentistInfo";
 
 const Contact = () => {
   const { dentistInfo } = useDentistInfo()
-  console.log(dentistInfo)
   return (
     <>
       <h1>Login</h1>
